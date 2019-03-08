2ed7be97d3fb2a3e0303789eca6795b063f0d356
wangxinjhy <2235447177@qq.com>
2019-03-08 11:40:15 +0800

    真题测试

--- src/main/java/com/zy/login/controller/LoginController.java
+++ src/main/java/com/zy/login/controller/LoginController.java
@@ -1,23 +1,20 @@
 package com.zy.login.controller;
 
 
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-
+import com.zy.login.pojo.Login;
+import com.zy.login.service.LoginService;
+import com.zy.util.YzmUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.zy.login.pojo.Login;
-import com.zy.login.service.LoginService;
-import com.zy.util.YzmUtil;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class LoginController {
@@ -60,6 +57,7 @@ public class LoginController {
 
 	@RequestMapping("/toIndex")
 	public String toIndex(){
+
 		return "index";
 	}
 
